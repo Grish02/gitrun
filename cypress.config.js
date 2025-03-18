ed97a5292e95083bd9077c4091686d0898e0739e
Grish02 <mgrishaa15@gmail.com>
2025-03-18 08:19:03 +0530

    report

--- cypress.config.js
+++ cypress.config.js
@@ -2,37 +2,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports/mocha",
+      quiet: true,
+      overwrite: false,
+      html: false,
+      json: true,
+    },
+  },
+  screenshotsFolder: "cypress/reports/mochareports/assets",
+
+  // Cypress test settings
   e2e: {
     setupNodeEvents(on, config) {
-      // Implement node event listeners here
+      
+      return config;
     },
-    baseUrl: 'http://localhost:8080', // Set your application's base URL
-    retries: 2, // Retry failed tests up to 2 times
-    video: true, // Enable video recording
-    screenshotOnRunFailure: true, // Capture screenshots on test failure
-    chromeWebSecurity: false, // Disable Chrome web security to prevent cross-origin issues
-    defaultCommandTimeout: 10000, // Increase timeout for commands
-    env: {
-      loginUrl: '/login', // Example of setting environment variables
-    },
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    specPattern: "cypress/e2e/**/*.cy.js", // Set the test file pattern
-
-    // Add Mochawesome reporter settings
-   
-  }
-  
-});
-module.exports = defineConfig({
-  e2e: {
-  "reporter": "mochawesome",
-  "reporterOptions": {
-  "charts": true,
-  "overwrite": false,
-  "html": false,
-  "json": true,
-  "reportDir": "cypress/reports/"
+    specPattern: "cypress/e2e/**/*.cy.js",
   },
-  }
-})
+});
