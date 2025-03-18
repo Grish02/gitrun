120f0ea4fab0abf1c15fa456f4e5234d5d8d37f6
Grish02 <mgrishaa15@gmail.com>
2025-03-18 08:56:46 +0530

    success

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,10 +16,10 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import addContext from "mochawesome/addContext";
+
 Cypress.on("test:after:run", (test, runnable) => {
-if (test.state === "failed") {
-const screenshot = `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title}
-(failed).png`;
-addContext({ test }, screenshot);
-}
+  if (test.state === "failed") {
+    const screenshot = `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
+    addContext({ test }, screenshot);
+  }
 });
